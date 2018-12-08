2510bf2ce27b9ba0ed6237efe1cb887b3bec64f0
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-08 16:15:08 +0900

    bind

--- app/src/main/java/jp/cordea/fluxdemo/ui/region/RegionFragmentBindsModule.kt
+++ app/src/main/java/jp/cordea/fluxdemo/ui/region/RegionFragmentBindsModule.kt
@@ -3,9 +3,19 @@ package jp.cordea.fluxdemo.ui.region
 import androidx.fragment.app.Fragment
 import dagger.Binds
 import dagger.Module
+import jp.cordea.fluxdemo.event.BaseSink
+import jp.cordea.fluxdemo.event.BaseSource
+import jp.cordea.fluxdemo.event.region.RegionAction
+import jp.cordea.fluxdemo.event.region.RegionDispatcher
 
 @Module
 interface RegionFragmentBindsModule {
     @Binds
     fun bindFragment(fragment: RegionFragment): Fragment
+
+    @Binds
+    fun bindSink(dispatcher: RegionDispatcher): BaseSink<RegionAction>
+
+    @Binds
+    fun bindSource(dispatcher: RegionDispatcher): BaseSource<RegionAction>
 }
