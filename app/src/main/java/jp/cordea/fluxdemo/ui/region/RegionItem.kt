3032c8e88675628916246af6668594f0e7b161ec
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-08 16:12:55 +0900

    simplify

--- app/src/main/java/jp/cordea/fluxdemo/ui/region/RegionItem.kt
+++ app/src/main/java/jp/cordea/fluxdemo/ui/region/RegionItem.kt
@@ -3,16 +3,10 @@ package jp.cordea.fluxdemo.ui.region
 import com.xwray.groupie.databinding.BindableItem
 import jp.cordea.fluxdemo.R
 import jp.cordea.fluxdemo.databinding.ListItemRegionBinding
-import javax.inject.Inject
 
-class RegionItem @Inject constructor(
+class RegionItem(
+    private val viewModel: RegionItemViewModel
 ) : BindableItem<ListItemRegionBinding>() {
-    private lateinit var viewModel: RegionItemViewModel
-
-    fun setViewModel(viewModel: RegionItemViewModel) = apply {
-        this.viewModel = viewModel
-    }
-
     override fun getLayout(): Int = R.layout.list_item_region
 
     override fun bind(viewBinding: ListItemRegionBinding, position: Int) {
