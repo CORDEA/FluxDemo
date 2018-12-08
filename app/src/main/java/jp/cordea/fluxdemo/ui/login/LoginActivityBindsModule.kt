c01180c4023911acaa4f89e53d1789d0eec87f8d
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-08 15:06:54 +0900

    bind

--- app/src/main/java/jp/cordea/fluxdemo/ui/login/LoginActivityBindsModule.kt
+++ app/src/main/java/jp/cordea/fluxdemo/ui/login/LoginActivityBindsModule.kt
@@ -3,9 +3,19 @@ package jp.cordea.fluxdemo.ui.login
 import android.app.Activity
 import dagger.Binds
 import dagger.Module
+import jp.cordea.fluxdemo.event.BaseSink
+import jp.cordea.fluxdemo.event.BaseSource
+import jp.cordea.fluxdemo.event.login.LoginAction
+import jp.cordea.fluxdemo.event.login.LoginDispatcher
 
 @Module
 interface LoginActivityBindsModule {
     @Binds
     fun bindActivity(activity: LoginActivity): Activity
+
+    @Binds
+    fun bindSink(dispatcher: LoginDispatcher): BaseSink<LoginAction>
+
+    @Binds
+    fun bindSource(dispatcher: LoginDispatcher): BaseSource<LoginAction>
 }
