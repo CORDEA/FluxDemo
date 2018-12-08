e651a77143e1d49aa491cae6b01f073039a5a07d
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-08 15:16:20 +0900

    store api key

--- app/src/main/java/jp/cordea/fluxdemo/event/login/LoginStore.kt
+++ app/src/main/java/jp/cordea/fluxdemo/event/login/LoginStore.kt
@@ -3,17 +3,20 @@ package jp.cordea.fluxdemo.event.login
 import io.reactivex.rxkotlin.ofType
 import io.reactivex.rxkotlin.withLatestFrom
 import io.reactivex.schedulers.Schedulers
+import jp.cordea.fluxdemo.KeyManager
 import jp.cordea.fluxdemo.di.ActivityScope
 import jp.cordea.fluxdemo.event.BaseSource
 import javax.inject.Inject
 
 @ActivityScope
 class LoginStore @Inject constructor(
+    private val keyManager: KeyManager,
     source: BaseSource<LoginAction>
 ) {
     private val onSubmit =
         source.reader.ofType<LoginAction.Clicked>()
             .withLatestFrom(source.reader.ofType<LoginAction.ChangedText>()) { _, query -> query.query }
+            .doOnNext { keyManager.set(it) }
             .map { if (it.isBlank()) LoginResult.Failure else LoginResult.Success }
             .subscribeOn(Schedulers.io())
 
