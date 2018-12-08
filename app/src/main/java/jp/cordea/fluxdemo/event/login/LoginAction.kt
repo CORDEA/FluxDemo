a48a851c469fbb22fffd374a9fc6874fde58df71
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-08 15:06:20 +0900

    implement store

--- app/src/main/java/jp/cordea/fluxdemo/event/login/LoginAction.kt
+++ app/src/main/java/jp/cordea/fluxdemo/event/login/LoginAction.kt
@@ -1,6 +1,6 @@
 package jp.cordea.fluxdemo.event.login
 
 sealed class LoginAction {
-    class ChangedText(query: String) : LoginAction()
+    class ChangedText(val query: String) : LoginAction()
     object Clicked : LoginAction()
 }
---
+++ app/src/main/java/jp/cordea/fluxdemo/event/login/LoginStore.kt
@@ -0,0 +1,22 @@
+package jp.cordea.fluxdemo.event.login
+
+import io.reactivex.rxkotlin.ofType
+import io.reactivex.rxkotlin.withLatestFrom
+import io.reactivex.schedulers.Schedulers
+import jp.cordea.fluxdemo.di.ActivityScope
+import jp.cordea.fluxdemo.event.BaseSource
+import javax.inject.Inject
+
+@ActivityScope
+class LoginStore @Inject constructor(
+    source: BaseSource<LoginAction>
+) {
+    private val onSubmit =
+        source.reader.ofType<LoginAction.Clicked>()
+            .withLatestFrom(source.reader.ofType<LoginAction.ChangedText>()) { _, query -> query.query }
+            .map { if (it.isBlank()) LoginResult.Failure else LoginResult.Success }
+            .subscribeOn(Schedulers.io())
+
+    fun onReady() = onSubmit.ofType<LoginResult.Success>()
+    fun onError() = onSubmit.ofType<LoginResult.Failure>()
+}
