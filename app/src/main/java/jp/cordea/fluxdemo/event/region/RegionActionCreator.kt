2b70c0ba71289faae69543129723d8cc787ac057
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-08 16:14:05 +0900

    implement action creator

---
+++ app/src/main/java/jp/cordea/fluxdemo/event/region/RegionActionCreator.kt
@@ -0,0 +1,13 @@
+package jp.cordea.fluxdemo.event.region
+
+import jp.cordea.fluxdemo.di.FragmentScope
+import jp.cordea.fluxdemo.event.BaseSink
+import javax.inject.Inject
+
+@FragmentScope
+class RegionActionCreator @Inject constructor(
+    private val sink: BaseSink<RegionAction>
+) {
+    fun init() = sink.dispatch(RegionAction.Init)
+    fun refresh() = sink.dispatch(RegionAction.Refresh)
+}
