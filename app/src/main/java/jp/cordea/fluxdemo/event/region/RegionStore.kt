3a94055d8be79216acb2d80979483c1953cec5a4
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-08 16:14:55 +0900

    implement store

---
+++ app/src/main/java/jp/cordea/fluxdemo/event/region/RegionStore.kt
@@ -0,0 +1,29 @@
+package jp.cordea.fluxdemo.event.region
+
+import io.reactivex.Flowable
+import io.reactivex.Single
+import io.reactivex.rxkotlin.ofType
+import jp.cordea.fluxdemo.di.FragmentScope
+import jp.cordea.fluxdemo.event.BaseSource
+import jp.cordea.fluxdemo.ui.region.RegionRepository
+import javax.inject.Inject
+
+@FragmentScope
+class RegionStore @Inject constructor(
+    private val repository: RegionRepository,
+    source: BaseSource<RegionAction>
+) {
+    private val init = source.reader.ofType<RegionAction.Init>().map { false }
+    private val refresh = source.reader.ofType<RegionAction.Refresh>().map { true }
+
+    private val fetch =
+        Flowable.merge(init, refresh)
+            .flatMapSingle {
+                repository.fetchRegion(it)
+                    .map<RegionResult> { RegionResult.Success(it) }
+                    .switchIfEmpty(Single.just(RegionResult.Failure))
+            }
+
+    fun onReady() = fetch.ofType<RegionResult.Success>().map { it.regions }
+    fun onError() = fetch.ofType<RegionResult.Failure>().map { Unit }
+}
